5dbf38a2af4d2bfc18bfec332857022979024ca3
tomifer13 <161241850+tomifer13@users.noreply.github.com>
2024-02-25 19:04:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas as pd
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 import matplotlib.pyplot as plt
 
 def obtener_index(url):
