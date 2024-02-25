7183b1f83d38ebb27b9effae8e6e5ab309909260
tomifer13 <161241850+tomifer13@users.noreply.github.com>
2024-02-25 18:10:23 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import matplotlib.pyplot as plt
+
+def obtener_index(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    index = driver.find_element(By.XPATH, '//*[@id="result-stats"]').text
+    index = index.split('Cerca de ')[1].split(' resultados')[0]
+    driver.quit()
+    # Eliminar los puntos de los números
+    index = index.replace('.', '').replace(',','').replace(' ','')
+    return int(index)
+
+
+def main():
+    st.title('Análisis de Páginas Indexadas')
+
+    urls_str = st.text_input("Ingrese las URLs separadas por coma (por ejemplo, ejemplo.com, prueba.com): ")
+
+    if st.button("Obtener URLs Indexadas"):
+        urls = [url.strip() for url in urls_str.split(',')]
+        
+        indexes = {}
+        
+        for url in urls:
+            search_url = f'https://www.google.com/search?q=site%3A{url}&oq=site%3A{url}&aqs=chrome..69i57j69i58.6029j0j1&sourceid=chrome&ie=UTF-8'
+            index = obtener_index(search_url)
+            indexes[url] = index
+            time.sleep(1)
+
+        df = pd.DataFrame.from_dict(indexes, orient='index', columns=['Páginas indexadas'])
+        df.sort_values(by='Páginas indexadas', ascending=False, inplace=True)
+
+        # Mostrar la tabla de datos
+        st.write(df)
+
+        # Crear un gráfico de torta
+        explode = [0.1] * len(df)  # Separación entre las porciones
+        colors = plt.cm.Set3(range(len(df)))  # Paleta de colores
+        fig, ax = plt.subplots()
+        ax.pie(df['Páginas indexadas'], labels=df.index, autopct='%1.1f%%', explode=explode, colors=colors, shadow=True, startangle=90)
+        ax.set_title('Páginas Indexadas')
+        ax.axis('equal')  # Asegura una forma circular
+        st.pyplot(fig)
+
+if __name__ == "__main__":
+    main()
